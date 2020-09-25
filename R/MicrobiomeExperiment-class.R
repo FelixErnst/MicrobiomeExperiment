dfde330c2975b5e732b49526c34df9ab8073b45c
Felix Ernst <felix.gm.ernst@outlook.com>
2020-09-25 08:40:42 +0200

    resync again against S4Vectors (0.27.12)

--- R/MicrobiomeExperiment-class.R
+++ R/MicrobiomeExperiment-class.R
@@ -45,7 +45,7 @@ setClass("MicrobiomeExperiment",
 )
 
 
-setMethod("vertical_slot_names", "MicrobiomeExperiment",
+setMethod("parallel_slot_names", "MicrobiomeExperiment",
           function(x) c("referenceSeq", callNextMethod())
 )
 
