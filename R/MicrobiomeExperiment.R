05fc8519ba5a60f58bce7b5cf8483c5f1c2e24e1
FelixErnst <felix.gm.ernst@outlook.com>
2020-11-03 13:19:50 +0100

    sync changes in TSE and mia

--- R/MicrobiomeExperiment.R
+++ R/MicrobiomeExperiment.R
@@ -1,49 +1,11 @@
 #' MicrobiomeExperiment Package.
 #'
-#' Package defining MicrobiomeExperiment class and methods
+#' Wrapper package for Microbiome analysis
 #'
 #' @name MicrobiomeExperiment-package
 #' @docType package
-#' @seealso \link[=MicrobiomeExperiment-class]{MicrobiomeExperiment} class
+#' @seealso \link[TreeSummarizedExperiment:MicrobiomeExperiment-class]{MicrobiomeExperiment} class
 NULL
 
 #' @import methods
-#' @import BiocGenerics
-#' @import S4Vectors
-NULL
-
-#' @title MicrobiomeExperiment datasets
-#'
-#' @description
-#' These datasets are conversions of the phyloseq datasets \code{GlobalPatterns}
-#' \code{enterotype}, \code{esophagus} and \code{soilrep}.
-#'
-#' \code{taxa} contains example taxonomic data derived from the
-#'   \code{metagenomeFeatures} package.
-#'
-#' @name MicrobiomeExperiment-datasets
-#' @docType data
-#' @keywords datasets
-#' @usage data(GlobalPatterns)
-"GlobalPatterns"
-#' @name MicrobiomeExperiment-datasets
-#' @usage data(enterotype)
-"enterotype"
-#' @name MicrobiomeExperiment-datasets
-#' @usage data(esophagus)
-"esophagus"
-#' @name MicrobiomeExperiment-datasets
-#' @usage data(soilrep)
-"soilrep"
-#' @name MicrobiomeExperiment-datasets
-#' @usage data(taxa)
-"taxa"
-
-#' MicrobiomeExperiment internals
-#'
-#' Internal functions which should only be used in MicrobiomeExperiment
-#'
-#' @keywords internal
-#'
-#' @name MicrobiomeExperiment-internal
 NULL
--- tests/testthat/test-0MicrobiomeExperiment-class.R
+++ tests/testthat/test-0MicrobiomeExperiment-class.R
@@ -3,29 +3,39 @@ context("MicrobiomeExperiment-class")
 test_that("MicrobiomeExperiment-class", {
     expect_true(validObject(MicrobiomeExperiment()))
     expect_true(validObject(MicrobiomeExperiment(SimpleList())))
-    data(taxa)
     sampleNames <- letters[1:4]
-    pd <- DataFrame(a=letters[1:4], b=1:4)
-    numcounts <- nrow(taxa) * 4
-    counts <- matrix(sample(1:1000, numcounts, replace=TRUE),
-                     nr = nrow(taxa), nc = 4)
+    cd <- DataFrame(a = letters[1:4], b = 1:4)
+    rd <- DataFrame(Kingdom = "Bacteria",
+                    Phylum = c("Firmicutes","Firmicutes","Firmicutes",
+                               "Bacteroidetes","Euryarchaeota"),
+                    Class = c("Bacilli","Bacilli","Bacilli","Saprospirae",
+                              "Methanobacteria"),
+                    Order = c("Bacillales","Lactobacillales","Lactobacillales",
+                              "Saprospirales","Methanobacteriales"),
+                    Family = c("Planococcaceae","Enterococcaceae","Enterococcaceae",
+                               "Chitinophagaceae","Methanobacteriaceae"),
+                    Genus = c("Staphylococcus",NA,"Melissococcus",
+                              "Sediminibacterium","Methanobrevibacter"))
+    counts <- matrix(sample(1:1000, nrow(rd) * nrow(cd), replace=TRUE),
+                     nr = nrow(rd),
+                     nc = nrow(cd))
     refSeq <- DNAStringSetList(one = DNAStringSet(c("A","A","A","A","A")),
                                two = DNAStringSet(c("A","A","A","A","A")))
     expect_error(MicrobiomeExperiment(assays = SimpleList(counts = counts),
-                                      rowData = taxa,
-                                      colData = pd,
+                                      rowData = rd,
+                                      colData = cd,
                                       referenceSeq = refSeq[list(1:5,1:4)]))
     expect_error(MicrobiomeExperiment(assays = SimpleList(counts = counts),
-                                      rowData = taxa,
-                                      colData = pd,
+                                      rowData = rd,
+                                      colData = cd,
                                       referenceSeq = refSeq[list(1:4,1:4)]))
     expect_error(MicrobiomeExperiment(assays = SimpleList(counts = counts),
-                                      rowData = taxa,
-                                      colData = pd,
+                                      rowData = rd,
+                                      colData = cd,
                                       referenceSeq = refSeq[[1L]][1:4]))
     me <- MicrobiomeExperiment(assays = SimpleList(counts = counts),
-                               rowData = taxa,
-                               colData = pd,
+                               rowData = rd,
+                               colData = cd,
                                referenceSeq = refSeq)
     expect_error(referenceSeq(me) <- refSeq[list(1:5,1:4)])
     expect_error(referenceSeq(me) <- refSeq[list(1:4,1:4)])
